4779cbd9409258f8e2132a27f0091d51397a23fe
Matthieu Grandrie <matthieu.grandrie@lyra-network.com>
2015-04-24 11:37:23 +0200

    Set updated_at on parent models whenever rating or stats are modified

--- statscollect_db/models/meta_model.py
+++ statscollect_db/models/meta_model.py
@@ -6,7 +6,7 @@ from django_extensions.db.fields import UUIDField
 class MetaModel(models.Model):
     uuid = UUIDField(unique=True)
     created_at = models.DateTimeField(editable=False, default=timezone.now())
-    updated_at = models.DateTimeField(default=timezone.now())
+    updated_at = models.DateTimeField(editable=False, default=timezone.now())
 
     class Meta:
         abstract = True
