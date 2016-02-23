e7f1ac73decfa8da942b96f778e6b72b2335a301
Matthieu Grandrie <matthieu.grandrie@lyra-network.com>
2016-02-23 14:31:03 +0100

    Fix orange scrapping : get cap

--- statscollect_scrap/scrappers/scrappers.py
+++ statscollect_scrap/scrappers/scrappers.py
@@ -308,7 +308,7 @@ class OrangeRatingsScrapper(BaseScrapper):
         result = []
         # homep = article[0].xpath('p[last()-1]/strong')
         # awayp = article[0].xpath('p[last()]/strong')
-        name_pattern = r'(?:puis )*([\w][\w|àéèäëâêiîïöôûüù\- ]+) \((?:[\d]+[^,\-]+(?:\-[\s])*)*$'
+        name_pattern = r'(?:puis )*([\w][\w|àéèäëâêiîïöôûüù\- ]+)[\s]*(?:\(cap\))*[\s]*\((?:[\d]+[^,\-]+(?:\-[\s])*)*$'
         next_is_away = False
         homep = None
         awayp = None
