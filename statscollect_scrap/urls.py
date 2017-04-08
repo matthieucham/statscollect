e96bd605363286f5ce94ef85c450f0d738bd8f07
matthieucham <matthieu.cham@gmail.com>
2017-04-08 22:39:35 +0200

    views for debug

--- statscollect_scrap/urls.py
+++ statscollect_scrap/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
-from .views import ScrapedDataSheetViewSet
+from . import views
 
 
 urlpatterns = [
-    url(r'^datasheets/$', ScrapedDataSheetViewSet.as_view({'get': 'list', 'post': 'create', }, suffix='List'),
-        name='datasheet-list'),
+    url(r'^ping/$', views.ping_view),
+    url(r'^datasheets/$', views.scraped_datasheet_detail),
     url(r'^datasheets/(?P<hash_url>[a-f0-9]{8,})/$',
-        ScrapedDataSheetViewSet.as_view({'get': 'retrieve', }), name='datasheet-detail'),
+        views.ScrapedDataSheetViewSet.as_view({'get': 'retrieve', }), name='datasheet-detail'),
 ]
--- statscollect_scrap/views.py
+++ statscollect_scrap/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.parsers import JSONParser
+from django.http import JsonResponse
 
 from .models import ScrapedDataSheet
 from .serializers import ScrapedDataSheetSerializer
@@ -9,3 +12,23 @@ class ScrapedDataSheetViewSet(viewsets.ModelViewSet):
     serializer_class = ScrapedDataSheetSerializer
     lookup_field = 'hash_url'
     permission_classes = (permissions.AllowAny, )
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny, ))
+def scraped_datasheet_detail(request):
+    data = JSONParser().parse(request)
+    serializer = ScrapedDataSheetSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data)
+    return JsonResponse(serializer.errors, status=400)
+
+
+@api_view(['GET', 'POST'])
+@permission_classes((permissions.AllowAny, ))
+def ping_view(request):
+    return JsonResponse({'ping': request.data}, safe=False)
