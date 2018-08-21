ce9e94d57dbb702cb0d7235f86f83e26fc35a692
Matthieu Grandrie <matthieu.grandrie@lyra-network.com>
2018-08-21 09:36:29 +0200

    special case for HDM date lookup

--- statscollect_scrap/lookups.py
+++ statscollect_scrap/lookups.py
@@ -84,14 +84,11 @@ class RatingsheetLookup(ModelLookup):
             sheet_choices = dict(
                 [(elem['hash_url'],
                   '%s=%s' % (elem['content']['home_team'], elem['content']['away_team'])) for elem in
-                 models.ScrapedDataSheet.objects.filter(
-                     match_date__range=(
-                         datetime.datetime.combine(gamesheet.match_date.date(),
-                                                   datetime.time.min),
-                         datetime.datetime.combine(gamesheet.match_date.date(),
-                                                   datetime.time.max),
-                     )).filter(Q(source__in=('FF',)) | Q(content__home_score=gamesheet.content['home_score'],
-                                                         content__away_score=gamesheet.content['away_score'], )).values(
+                 models.ScrapedDataSheet.objects.filter(Q(source__in=('HDM',)) | Q(match_date__range=(
+                 datetime.datetime.combine(gamesheet.match_date.date(), datetime.time.min),
+                 datetime.datetime.combine(gamesheet.match_date.date(), datetime.time.max)), )).filter(
+                     Q(source__in=('FF',)) | Q(content__home_score=gamesheet.content['home_score'],
+                                               content__away_score=gamesheet.content['away_score'], )).values(
                      'hash_url',
                      'content')])
             # sheet_search_key = '%s=%s' % (gamesheet.content['home_team'], gamesheet.content['away_team'])
