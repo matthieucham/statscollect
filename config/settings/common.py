5a2237adc5837775594d6f7f6861c60072cf1780
Matthieu Grandrie <matthieu.grandrie@lyra-network.com>
2018-08-23 17:52:37 +0200

    Restore protections on resources

--- config/settings/common.py
+++ config/settings/common.py
@@ -305,7 +305,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
